b2a61601943dd740b271a782c80111e47da07ead
Hung Quach <quachio@icloud.com>
2016-07-14 20:22:23 -0700

    finish webpack tutorial

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,11 @@ module.exports = {
   output: {
     path: './dist',
     filename: 'app.bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
   }
 }
